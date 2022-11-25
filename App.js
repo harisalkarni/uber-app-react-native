3edb7d9464624592833157aeef92585e338ae01e
harisalkarni <haris.alkarni@gmail.com>
2022-11-25 18:13:26 +0700

    2nd Update

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+
 import { store } from './store/store';
+import "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 // 1) Set up a redux store
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer >
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false,}} />
+            <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false,}} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from "tailwind-react-native-classnames"
+import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text style={tw`p-10 text-red-500`}>HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image style={{width: 100, height: 100, resizeMode: "contain",}} source={{uri: "https://links.papareact.com/gzs", }} />
+        <NavOptions />
+      </View>
     </SafeAreaView>
   )
 }
